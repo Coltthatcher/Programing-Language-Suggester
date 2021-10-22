32bceec728cdc3d4653abe36c25ac71e4daf218a
Colt Thatcher <colt.thatcher303@gmail.com>
2021-10-22 15:44:22 -0700

    Add some style.css to make the page stand out

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
+  $("#survey").sumbit(function(event) {
+    event.preventDefault();
+    const question1 = document.survey.question1.value;
+    const question2 = document.survey.question2.value;
+    const question3 = document.survey.question3.value;
+    const question4 = document.survey.question4.value;
+    const question5 = document.survey.question5.value;
 
-  const question1 = document.survey.question1.value;
-  const question2 = document.survey.question2.value;
-  const question3 = document.survey.question3.value;
-  const question4 = document.survey.question4.value;
-  const question5 = document.survey.question5.value;
-
-  let final = (question1 + question2 + question3 + question4 + question5);
+    let final = (question1 + question2 + question3 + question4 + question5);
 
   
 
@@ -20,11 +21,11 @@ $(document).ready(function() {
     
 
 
+    $("#story").show();
 
-
-    event.preventDefault();
+    
   });
-
+});
 
 
 
