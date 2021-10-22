24dc32c9faa364b2dcac9ca6a640de305c42b9ce
Colt Thatcher <colt.thatcher303@gmail.com>
2021-10-22 13:47:35 -0700

    Add branch for question1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+$(document).ready(function() {
+
+  var question1 = document.survey.question1.value;
+  var question2 = document.survey.question2.value;
+  var question3 = document.survey.question3.value;
+  var question4 = document.survey.question4.value;
+  var question5 = document.survey.question5.value;
+
+    if (question1 == "yes") {
+      $("ruby").show();
+    } else if (question1 == "no") {
+      $("c#").show();
+    }
+
+
+
+});
+
+
 
 
 
