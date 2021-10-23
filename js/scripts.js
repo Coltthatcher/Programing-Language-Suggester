bd92c3cff0b91255b8b72586689a628af45f315a
Colt Thatcher <colt.thatcher303@gmail.com>
2021-10-22 17:05:25 -0700

    change layout of page cannot get entire requierments to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("#survey").submit(function(event) {
     event.preventDefault();
-    const question1 = document.survey.question1.value;
+    const question1 = parseInt($("select#Q1").val());
     const question2 = document.survey.question2.value;
     const question3 = document.survey.question3.value;
     const question4 = document.survey.question4.value;
